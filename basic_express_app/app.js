63ca8aad57afba08e3b7375a62202c0d59aa7fc8
Petro Lesapiti <petrolesapiti@gmail.com>
2024-12-09 23:17:51 +0300

    clean the directory

--- basic_express_app/app.js
+++ basic_express_app/app.js
@@ -1,17 +1,6 @@
 const express = require("express");
 
 const app = express();
-//Middlewares
-const { logger } = require("./logger");
-const authorize = require("./authorize");
-app.use([logger, authorize]);
-
-app.get("/", (req, res) => {
-  res.send("Home page");
-});
-app.get("/about", (req, res) => {
-  res.send("About page");
-});
 
 app.listen(5000, () => {
   console.log("Listening to port 5000");
